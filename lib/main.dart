7bb6bb5b658bc8340b03449b1c9808102cacfaf2
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-03 18:53:44 +0200

    add a font and TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('hello'),
       centerTitle: true,
+      backgroundColor: Colors.brown[300],
     ),
     body: Center(
-      child: Text('this is the body'),
+      child: Text(
+          'this is the body',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
       child: Text('click'),
+      backgroundColor: Colors.brown[300],
     ),
   ),
 ));
