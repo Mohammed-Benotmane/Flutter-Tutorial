653a11784b5b00377f6f92a9a719de289702c63a
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-03 18:39:28 +0200

    adding body and floating button

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
       title: Text('hello'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('this is the body'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
     ),
   ),
 ));
