e31b976e25d3a4693f38cb7f9461ede1fec6bf41
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-04 17:49:28 +0200

    start a new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: NinjaCard(),
     ));
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('hello'),
-        centerTitle: true,
-        backgroundColor: Colors.brown[300],
-      ),
-      body:Row(
-        children: <Widget>[
-          Expanded(
-            child: Image.asset('assets/costume2.jpg'),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('1'),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: Text('2'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: Text('3'),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.brown[300],
-      ),
+
     );
   }
 }
